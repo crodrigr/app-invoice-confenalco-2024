a850ddf3620c60658e551665e4962546918f433e
crodrigr <crodrigr@gmail.com>
2024-07-08 19:51:24 -0500

    conexión con mariadb

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("Juan", "Perez", "juan.perez@example.com", "Calle 123, Ciudad", "1234567890", "1995-02-23");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("María", "Gómez", "maria.gomez@example.com", "Avenida 456, Pueblo", "9876543210", "1992-03-25");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("Carlos", "López", "carlos.lopez@example.com", "Carrera 789, Villa", "5678901234", "1985-03-25");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("Laura", "Martínez", "laura.martinez@example.com", "Camino 234, Campo", "6789012345", "1988-03-25");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("Pedro", "Sánchez", "pedro.sanchez@example.com", "Ruta 567, Aldea", "7890123456", "1995-03-25");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("Ana", "Rodríguez", "ana.rodriguez@example.com", "Sendero 890, Pueblo", "8901234567", "1993-03-25");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento) VALUES ("Diego", "Hernández", "diego.hernandez@example.com", "Av. Principal 1234, Ciudad", "9012345678", "1987-03-25");
+
+INSERT INTO clientes (nombres, apellidos, email, direccion, celular, fecha_nacimiento)  VALUES ("Sofía", "García", "sofia.garcia@example.com", "Callejón 4567, Pueblo", "3456789012", "1991-03-25");
